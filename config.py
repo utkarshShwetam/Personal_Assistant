a1b959907339b6d42bb0bb3476042e634ea1fe12
Utkarsh Raj Shwetam <42714802+utkarshShwetam@users.noreply.github.com>
2020-05-04 01:05:26 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-API_KEY='a141fb9d6ac6d834d9ad673f148d9fb2'
-WEB_LINK='https://www.ipinfo.io'
-NEWS_URL="https://news.google.com/news/rss"
-WEBLOC='C://Program Files (x86)//Google//Chrome//Application//chrome.exe %s'
-MUSIC_DIR="E:\\my file\\MMC\\Music"
-URL_SEARCH="http://google.com/?#q="
